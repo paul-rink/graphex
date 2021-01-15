c6e95e7e8e82d21d2ebe05b15b8526f9cb951455
Kai <52977355+NikaLost@users.noreply.github.com>
2021-01-15 23:22:38 +0100

    Added skeleton for Dijkstra

--- src/main/java/graphex2021/model/Dijkstra.java
+++ src/main/java/graphex2021/model/Dijkstra.java
@@ -1,12 +1,26 @@
 package graphex2021.model;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
+ * This class simulates the Dijksta-Algorithm.
+ * TODO generic in Dijkstra (and other classes..)? Better defining in GXGraph?
+ *
  * @author D. Flohs, K. Marquardt, P. Rink
  * @version 1.0 14.01.2021
  */
-public class Dijkstra implements Algorithm {
+public class Dijkstra<V,E> implements Algorithm {
+    private GXGraph<GXVertex<V>,GXEdge<E,V>> g;
+    private GXVertex<V> start;
+    private int[] dist;
+    private GXVertex<V>[] prev; //TODO int-array with id or Vertex-list
+    private PriorityQueue<GXVertex<V>> unmarked;
+    private List<Step> steps;
+
+    public Dijkstra(GXGraph g) {
+    }
+
     @Override
     public List<Step> getSequence(GXGraph g) {
         return null;
@@ -26,4 +40,13 @@ public class Dijkstra implements Algorithm {
     public boolean hasEndingVertex() {
         return false;
     }
+
+    private void calculateShortestPath() {
+
+    }
+
+    private void updateDistance() {
+
+    }
+
 }
