33149543121b9bc96ccc6c024217ffc3f1e15664
Kai <52977355+NikaLost@users.noreply.github.com>
2021-01-15 22:03:02 +0100

    Added Algorithm interface with JavaDoc (TODO)

--- src/main/java/graphex2021/model/Dijkstra.java
+++ src/main/java/graphex2021/model/Dijkstra.java
@@ -1,8 +1,29 @@
 package graphex2021.model;
 
+import java.util.List;
+
 /**
  * @author D. Flohs, K. Marquardt, P. Rink
  * @version 1.0 14.01.2021
  */
 public class Dijkstra implements Algorithm {
+    @Override
+    public List<Step> getSequence(GXGraph g) {
+        return null;
+    }
+
+    @Override
+    public boolean isRevealed() {
+        return false;
+    }
+
+    @Override
+    public boolean hasStartingVertex() {
+        return false;
+    }
+
+    @Override
+    public boolean hasEndingVertex() {
+        return false;
+    }
 }
