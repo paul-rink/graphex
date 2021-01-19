e11f16dbb6d1857b2b4a69877663ccbc0b8bd62f
Kai <52977355+Kai@users.noreply.github.com>
2021-01-19 12:09:52 +0100

    added empty methods for DisplayModel

--- src/main/java/graphex2021/model/DisplayModel.java
+++ src/main/java/graphex2021/model/DisplayModel.java
@@ -1,8 +1,42 @@
 package graphex2021.model;
 
+import java.util.List;
+
 /**
  * @author D. Flohs, K. Marquardt, P. Rink
  * @version 1.0 14.01.2021
  */
 public class DisplayModel {
+    private List<Step> userSteps;
+    private List<Step> algoSteps;
+    private Algorithm algo;
+    private GXGraph graph;
+
+    DisplayModel() { }
+
+    public GXEdge nexStep() {
+        return null;
+    }
+
+    public void markEdge(GXEdge edge) { }
+
+    public void markVertex(GXVertex vertex) { }
+
+    public GXGraph getState() { return null; }
+
+    public void undo() { }
+
+    private void makeVisible(GXEdge edge) { }
+
+    private void makeVisible(GXVertex vertex) { }
+
+    private GXVertex getStartVertex() { return null; }
+
+    private GXVertex getFinalVertex() { return null; }
+
+    private void updateVisibleGraph() { }
+
+    private Step getLastUserStep() { return null; }
+
+    private void removeLastUserStep() { }
 }
