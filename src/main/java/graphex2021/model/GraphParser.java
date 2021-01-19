c319d0f2d834630aadda50418e2981ee25cc2593
Paul Rink <paul@herrmann-rink.de>
2021-01-19 12:26:07 +0100

    GraphParser: slightly changed vertexparse, implemented edgeparse, startvertexparse, endvertexparse and helpers
    help methods include:
    methods to create a JSONObject from a file that need to be read
    a counter to create unique ids

--- src/main/java/graphex2021/model/GraphParser.java
+++ src/main/java/graphex2021/model/GraphParser.java
@@ -3,16 +3,13 @@ package graphex2021.model;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import org.json.*;
 
 
 /**
  * Class to parse JSON files which specify a graph
- * as only one parser is ever needed this is a singleton
  *
  * @author D. Flohs, K. Marquardt, P. Rink
  * @version 1.0 14.01.2021
@@ -48,15 +45,9 @@ public class GraphParser {
     public Collection<GXVertex> parseVertices(File input) {
         freeVertexId = 0;
         freeEdgeId = 0;
+        Collection<GXVertex> vertexList = new <GXVertex>ArrayList();
 
-        Collection<GXVertex> vertexList = new <GXVertex>LinkedList();
-        String inputFile = "";
-        try {
-            inputFile = Files.readString(input.toPath());
-        } catch (IOException e) {
-            //TODO shouldnt happen
-        }
-        JSONObject graphObject = new JSONObject(inputFile);
+        JSONObject graphObject = getJsonObject(input);
         JSONArray verticesArray = graphObject.getJSONArray("vertices");
         for(int i = 0; i < verticesArray.length(); i++) {
             JSONObject jsonVertex = verticesArray.getJSONObject(i);
@@ -71,22 +62,122 @@ public class GraphParser {
         return vertexList;
     }
 
-    public Collection<GXEdge> parseEdges(File input) {
+    /**
+     * method to parse the vertices from a file
+     * @param input the file from which the vertices are parsed
+     * @param vertices the collection of vertices the edges will connect
+     * @return a Collection of edges for the graph
+     */
+    public Collection<GXEdge> parseEdges(File input, Collection<GXVertex> vertices) {
+        Collection<GXEdge> edgeList = new <GXEdge> ArrayList();
+
+        JSONObject graphObject = getJsonObject(input);
+        JSONArray edgeArray = graphObject.getJSONArray("edges");
+
+        for (int i = 0; i < edgeArray.length(); i++) {
+            JSONObject jsonEdge = edgeArray.getJSONObject(i);
+            String vertexOneName = jsonEdge.getString("vertex1");
+            String vertexTwoName = jsonEdge.getString("vertex2");
+            int edgeWeight = jsonEdge.getInt("weight");
+            //find first vertex
+            GXVertex firstVertex = findMatchingVertex(vertexOneName, vertices);
+            GXVertex secondVertex = findMatchingVertex(vertexTwoName, vertices);
+            if(firstVertex!= null && secondVertex != null) {
+                int edgeId = getNextEdgeId();
+                GXEdge edge = new GXEdge(firstVertex, secondVertex, Integer.toString(edgeWeight), edgeWeight, edgeId);
+                edgeList.add(edge);
+            }
+        }
         return null;
     }
 
-    public GXVertex parseStarting(File input){
-        return null;
+    /**
+     * method that parses which vertex is designated as the start vertex in the file
+     * @param input the file in which the graph is specified
+     * @param vertices the list of vertices in the graph
+     * @return
+     */
+    public GXVertex parseStarting(File input, Collection<GXVertex> vertices){
+        JSONObject graphObject = getJsonObject(input);
+        String startName =  graphObject.getString("startVertex");
+        GXVertex startVertex = findMatchingVertex(startName, vertices);
+        return  startVertex;
     }
 
-    public GXVertex parseEnding(File input) {
-        return null;
+    /**
+     * method that parses which vertex is designated as the end vertex in the file
+     * @param input the file in which the graph is specified
+     * @param vertices the list of vertices in the graph
+     * @return
+     */
+    public GXVertex parseEnding(File input, Collection<GXVertex> vertices) {
+        JSONObject graphObject = getJsonObject(input);
+        String endName =  graphObject.getString("endVertex");
+        GXVertex endVertex = findMatchingVertex(endName, vertices);
+        return  endVertex;
     }
 
+    /**
+     * method that takes a file and creates a JSONObject from it
+     * @param input the file which is being read
+     * @return a JSONObject which is defined in the file
+     */
+    private JSONObject getJsonObject(File input) {
+        //TODO make a JSON matcher to test if the file is actually a graph
+        String inputFile = readFromFile(input);
+        return new JSONObject(inputFile);
+    }
+
+
+    /**
+     * method to read a string from a given File
+     * @param input the File the string should be read from
+     * @return a String with the contents of the File
+     */
+    private String readFromFile(File input) {
+        String output ="";
+        try {
+            output = Files.readString(input.toPath());
+        } catch (IOException e) {
+            //TODO shouldnt happen
+        }
+        return output;
+    }
+
+    /**
+     * method that takes a String and a Collection of vertices and tries to find a vertex
+     * with a matching name/element
+     * @param vertexName the name of the vertex being searched
+     * @param vertices collection of vertices
+     * @return either a vertex with matching name or null
+     */
+    private GXVertex findMatchingVertex(String vertexName, Collection<GXVertex> vertices){
+        //TODO if element is the name and its not unique this will have problems (should we allow vertices with the same name?)
+        Iterator it = vertices.iterator();
+        GXVertex matchingVertex = null;
+        while(it.hasNext()) {
+            GXVertex vertex = (GXVertex) it.next();
+            if (vertex.element().equals(vertexName)) {
+                matchingVertex = vertex;
+            }
+        }
+        return  matchingVertex;
+    }
+
+    /**
+     * returns an integer and counts up one
+     * this is used to identify vertices
+     * @return the next free id
+     */
     private int getNextVertexId() {
         return freeVertexId++;
     }
 
+    /**
+     * returns an integer and counts up one
+     * this is used to identify edges
+     * @return the next free id
+     */
     private int getNextEdgeId() {
         return  freeEdgeId++;
     }
