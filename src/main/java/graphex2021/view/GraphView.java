5f1a0e90a8d732a24f790106e98113a381122c6f
Dominik <dominik.flohs@googlemail.com>
2021-01-20 12:50:40 +0100

    Fixed getStarting/EndingVErtex returning the wrong vertices. Made Graphview extend the correct class Smartgraphpanel

--- src/main/java/graphex2021/view/GraphView.java
+++ src/main/java/graphex2021/view/GraphView.java
@@ -1,9 +1,14 @@
 package graphex2021.view;
 
-import com.brunomnsilva.smartgraph.graphview.SmartLabel;
+import com.brunomnsilva.smartgraph.graph.Graph;
+import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
 
 
-public class GraphView extends SmartLabel {
+
+public class GraphView extends SmartGraphPanel {
 
 
+    public GraphView(Graph theGraph) {
+        super(theGraph);
+    }
 }
