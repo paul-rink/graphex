eb0c79a062b7878a03464fb3a437a0d5beb52ca3
Kai <52977355+Kai@users.noreply.github.com>
2021-01-26 13:31:25 +0100

    added small constructor test for GXGraph

--- src/test/java/graphex2021/model/GXGraphTest.java
+++ src/test/java/graphex2021/model/GXGraphTest.java
@@ -2,13 +2,31 @@ package graphex2021.model;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
 
 import static org.junit.Assert.*;
 
 public class GXGraphTest {
+    private static final File graphFile = new File("src/test/resources/GraphData/exampleGraph.json");
+    GXGraph g;
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testGXGraphConstructor() {
+        try {
+            g = new GXGraph(graphFile);
+        } catch (ElementNotInGraphException e) {
+            e.printStackTrace();
+        }
+        assertEquals(14,g.vertices().size());
+        assertEquals(23,g.edges().size());
+        assertEquals("S", g.getStartingVertex().element());
+        assertEquals("Z", g.getEndingVertex().element());
     }
 
     @After
