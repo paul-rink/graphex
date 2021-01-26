c575f28fc3422005f40961574356511bb4568a3c
Dominik <dominik.flohs@googlemail.com>
2021-01-26 14:00:16 +0100

    Fixed the StyleSheet and properties not being loaded on GraphView creation.

--- src/main/java/graphex2021/view/GraphView.java
+++ src/main/java/graphex2021/view/GraphView.java
@@ -1,6 +1,5 @@
 package graphex2021.view;
 
-import com.brunomnsilva.smartgraph.graph.Graph;
 import com.brunomnsilva.smartgraph.graphview.SmartCircularSortedPlacementStrategy;
 import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
 import com.brunomnsilva.smartgraph.graphview.SmartGraphProperties;
@@ -8,14 +7,26 @@ import graphex2021.model.GXGraph;
 import graphex2021.model.Observer;
 import graphex2021.model.Subject;
 
+
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
 
 
 public class GraphView extends SmartGraphPanel<String, String> implements Observer {
 
+    //TODO check best Filepath separator
+    private static final File STYLESHEET = new File( "src" + File.separator + "main"
+            + File.separator + "resources" + File.separator + "graphex2021"
+            + File.separator + "smartgraph.css");
+    private static final File PROPERTIES = new File("src" + File.separator + "main"
+            + File.separator + "resources" + File.separator + "graphex2021"
+            + File.separator + "smartgraph.properties");
 
-    public GraphView() {
-        super(new GraphAdapter() , new SmartGraphProperties(), new SmartCircularSortedPlacementStrategy());
+    public GraphView() throws FileNotFoundException {
+        super(new GraphAdapter() , new SmartGraphProperties(new FileInputStream(PROPERTIES)),
+                new SmartCircularSortedPlacementStrategy(), STYLESHEET.toURI());
     }
 
     @Override
