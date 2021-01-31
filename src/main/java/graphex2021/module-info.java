68f551ce38c735551cdc4f6600ca652e230cc61b
Paul Rink <paul@herrmann-rink.de>
2021-01-31 15:07:00 +0100

    readded the module info. works like this as well

---
+++ src/main/java/graphex2021/module-info.java
@@ -0,0 +1,14 @@
+module graphex2021 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires SmartGraph;
+    requires org.json;
+    requires org.everit.json.schema;
+
+    opens graphex2021.controller to javafx.fxml;
+    exports graphex2021.controller;
+    exports graphex2021;
+    exports graphex2021.model;
+    exports graphex2021.view;
+
+}
