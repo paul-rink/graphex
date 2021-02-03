a70948185b2bebf39cde419d48bc959d06308666
Kai <52977355+Kai@users.noreply.github.com>
2021-02-03 11:14:23 +0100

    added enum for possible labels of vertices

---
+++ src/main/java/graphex2021/model/VertexLabel.java
@@ -0,0 +1,9 @@
+package graphex2021.model;
+
+public enum VertexLabel {
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+
+    public String toString() {
+        return this.toString();
+    }
+}
