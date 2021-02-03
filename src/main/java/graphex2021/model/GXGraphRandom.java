765c1173fcf7d3ad5945caf184aa7f1295bfd3dd
Kai <52977355+Kai@users.noreply.github.com>
2021-02-03 11:15:23 +0100

    added GraphGenerator with method generateVertices

---
+++ src/main/java/graphex2021/model/GXGraphRandom.java
@@ -0,0 +1,51 @@
+package graphex2021.model;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * @author D. Flohs, K. Marquardt, P. Rink
+ * @version 1.0 14.01.2021
+ */
+public class GXGraphRandom extends GXGraph {
+    /**
+     * is the maximal number of vertices a random graph can exist of depending on Labels existing for vertices.
+     */
+    public static final int MAX_NUMBER_VERTICES = VertexLabel.values().length;
+    /**
+     * is the maximal number of vertices a random graph can exist of depending on Labels existing for vertices.
+     */
+    public static final int MIN_NUMBER_VERTICES = 1;
+
+    public GXGraphRandom(int numVertices, float p) {
+        super();
+        if (numVertices > MAX_NUMBER_VERTICES || numVertices < MIN_NUMBER_VERTICES) {
+            throw new IllegalArgumentException("Maximum amount of vertices = " + MAX_NUMBER_VERTICES);
+        }
+        generateVertices(numVertices);
+    }
+
+    /**
+     * Generates random vertices.
+     * @param num number if vertices
+     * @return list of generated vertices
+     */
+    private LinkedList<GXVertex> generateVertices(int num) {
+        //array of all possible vertex labels, that are accessed via counter
+        VertexLabel[] labels = VertexLabel.values();
+        //list of all created vertices
+        LinkedList<GXVertex> newVertices = new LinkedList<>();
+        //used to generate random positions
+        Random r = new Random();
+        //counter that will represent id of a vertex
+        int counter = 0;
+        while (counter < num) {
+            int rndX = r.nextInt(GXPosition.POSITION_RANGE);
+            int rndY = r.nextInt(GXPosition.POSITION_RANGE);
+            GXPosition rndPosition = new GXPosition(rndX, rndY);
+            GXVertex newVertex = new GXVertex(labels[counter].toString(), counter, rndPosition);
+            counter++;
+        }
+        return newVertices;
+    }
+}
