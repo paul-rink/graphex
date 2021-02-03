6cf5e22e5453e9755ed745efe0cbde26843e6aee
Kai <52977355+Kai@users.noreply.github.com>
2021-02-03 11:30:03 +0100

    fixed toString loop

--- src/main/java/graphex2021/model/VertexLabel.java
+++ src/main/java/graphex2021/model/VertexLabel.java
@@ -1,9 +1,5 @@
 package graphex2021.model;
 
 public enum VertexLabel {
-    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
-
-    public String toString() {
-        return this.toString();
-    }
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
 }
