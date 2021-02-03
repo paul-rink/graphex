6b391f149a9b1025fe507fcd626320a178c03caf
Kai <52977355+Kai@users.noreply.github.com>
2021-02-03 22:04:39 +0100

    added static variable to main for prop windows scene

--- src/main/java/graphex2021/Main.java
+++ src/main/java/graphex2021/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Scene GRAPH_PROPERTY_SCENE = null;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,16 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //init primary stage: GUI
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GraphexMainWindow.fxml"));
-
         Parent root = loader.load();
-
         primaryStage.setTitle("GraphEx");
-
         primaryStage.setScene(new Scene(root));
-
-
         primaryStage.show();
+
+        //init Scene for property window for random graph
+        FXMLLoader loaderPropWin = new FXMLLoader(getClass().getResource("PropWin.fxml"));
+        GRAPH_PROPERTY_SCENE = new Scene(loaderPropWin.load());
 
         Controller graphController = loader.getController();
         graphController.init();
