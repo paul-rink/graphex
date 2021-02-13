a4285851567d9b6c7784452c96e75f53902b3e65
Paul Rink <paul@herrmann-rink.de>
2021-02-13 18:48:42 +0100

    second part of tests for used methods

--- src/test/java/graphex2021/model/GXVertexTest.java
+++ src/test/java/graphex2021/model/GXVertexTest.java
@@ -1,27 +1,30 @@
 package graphex2021.model;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 
 import static org.junit.Assert.*;
 
 public class GXVertexTest {
     private static GXVertex testVertex;
+    private static GXPosition testPosition;
     private static GXVertex secondVertex;
     private static GXEdge prevEdge;
+
     @Before
     public void setup() {
         //create a Vertex with expected values
-        testVertex = new GXVertex("A", 5,new GXPosition(3,5));
+        testPosition = new GXPosition(3,5);
+        testVertex = new GXVertex("A", 5,testPosition);
         secondVertex = new GXVertex("B", 3, new GXPosition(2,3));
         prevEdge = new GXEdge(testVertex, secondVertex, "4", 4, 1);
     }
 
     @After
     public void cleanUp() {
+        testPosition = null;
         testVertex = null;
+        secondVertex = null;
+        prevEdge = null;
     }
 
 
@@ -47,9 +50,19 @@ public class GXVertexTest {
     }
 
     @Test
+    public void testDefaultDistance() {
+        assertEquals(0, testVertex.getCurrentDistance());
+    }
+
+    @Test
     public void testMark() {
         testVertex.mark(3, prevEdge);
         assertTrue(checkMark());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testMarkNull() {
+        testVertex.mark(3, null);
     }
 
     private boolean checkMark() {
@@ -87,4 +100,58 @@ public class GXVertexTest {
         return result;
     }
 
+    @Test
+    public void testSetCurrentDistance() {
+        testVertex.setCurrentDistance(1);
+        assertEquals(1, testVertex.getCurrentDistance());
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(5, testVertex.getId());
+    }
+
+    @Test
+    public void testGetPosition() {
+        assertTrue(testPosition.equals(testVertex.getPosition()));
+    }
+
+    @Test
+    public void testEqualsNull() {
+        assertFalse(testVertex.equals(null));
+    }
+
+    @Test
+    public void testEquals() {
+        assertTrue(testVertex.equals(new GXVertex("A", 5, new GXPosition(3,3))));
+    }
+
+    @Test
+    public void testDefaultType() {
+        GXVertexType testType = testVertex.getStartOrEnd();
+        assertEquals(GXVertexType.NORMAL, testType);
+    }
+    @Test
+    public void testSetType() {
+        testVertex.setType(GXVertexType.STARTING);
+        assertEquals(GXVertexType.STARTING, testVertex.getStartOrEnd());
+    }
+
+    @Test //TODO what is the expected behaviour here?
+    public void testSetTypeNull() {
+        testVertex.setType(null);
+        //assertEquals(GXVertexType.NORMAL, testVertex.getStartOrEnd());
+        assertEquals(null, testVertex.getStartOrEnd());
+    }
+
+    @Test
+    public void testDefaultIsHint() {
+        assertFalse(testVertex.isHint());
+    }
+
+    @Test
+    public void testSetHint() {
+        testVertex.setHint(true);
+        assertTrue(testVertex.isHint());
+    }
 }
