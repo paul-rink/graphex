a75c67c97dfc67525ef0af8040883927344f4b13
Dominik <dominik.flohs@googlemail.com>
2021-02-15 17:36:22 +0100

    Added some more test cases for insertVertex

--- src/test/java/graphex2021/model/GXGraphTest.java
+++ src/test/java/graphex2021/model/GXGraphTest.java
@@ -32,12 +32,48 @@ public class GXGraphTest {
 
 
     /**
-     * Tests the case if null is passed to the insert vertex method
+     * Tests the case if null is passed to the insert vertex method. Should return null
      */
     @Test
-    public void testInsertVerticesNulL() {
+    public void testInsertVerticesNull() {
         GXVertex vertex = null;
         assertNull(exampleGraph.insertVertex(vertex));
+    }
+
+    /**
+     * If there already is a vertex with the same ID as the one of the vertex inserted the vertex already in the graph
+     * should be returned
+     */
+    @Test
+    public void testInsertVertexIDAlreadyIn() {
+        GXVertex vertex = new GXVertex("A", 1, null);
+
+        GXVertex vertex1 = exampleGraph.getVertex(1);
+        GXVertex newVertex1 = exampleGraph.insertVertex(vertex);
+
+        assertEquals(vertex1, newVertex1);
+    }
+
+    /**
+     * Adding a new vertex with an unused id should add the vertex to the graph
+     *
+     */
+    @Test
+    public void testInsertVertexNewVertex() {
+        GXVertex vertex  = new GXVertex("M", 14, null);
+        assertEquals(exampleGraph.insertVertex(vertex), vertex);
+        assertEquals(vertex, exampleGraph.getVertex(14));
+    }
+
+    /**
+     * Adding new Vertex with the same Element as an other vertex
+     *
+     */
+    @Test
+    public void testInsertVertexElementAlreadyIn() {
+        GXVertex vertex =  new GXVertex("A", 14, null);
+        assertEquals(exampleGraph.insertVertex(vertex), vertex);
+        assertEquals(vertex, exampleGraph.getVertex(14));
     }
 
     @After
