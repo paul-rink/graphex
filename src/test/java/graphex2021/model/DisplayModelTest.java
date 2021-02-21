8f9eb078c28a83be9c93abade0ef20b883e1e9e6
Dominik <dominik.flohs@googlemail.com>
2021-02-22 00:19:55 +0100

    Added first mocked Test for the placement strategy

---
+++ src/test/java/graphex2021/model/DisplayModelTest.java
@@ -0,0 +1,17 @@
+package graphex2021.model;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class DisplayModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
